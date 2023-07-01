426c0389760c75ab01db928fac03d064a1de6c86
Belhadj Ahmed Walid <belhadj.ahmedwalid1@gmail.com>
2023-07-01 21:13:12 +0100

    card component made

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
+import Card from './Components/Card'
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <React.Fragment>
       <Navbar />
       <Hero />
+      <Card />
     </React.Fragment>
   )
 }
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,10 +1,17 @@
-
-
+import card_photo from '../Assets/katie-zaferes.png'
+import star_photo from '../Assets/star.png'
 export default function Card(){
     return (
-        <div>
-        <img></img>
-
+        <div className="card">
+        <img className='card--img' src={card_photo}></img>
+        <div className='card--stats'>
+            <img className='card--star' src={star_photo} />
+            <span>5.0</span>
+            <span className='grey'>(6) •</span>
+            <span className='grey'>USA</span>
+        </div>
+        <p>Life Lessons with Katie Zeferes</p>
+        <p> <span className='bold'>From $136</span> / Person</p>
         </div>
     )
 }
